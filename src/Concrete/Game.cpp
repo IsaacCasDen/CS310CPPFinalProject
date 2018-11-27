cca07d810b7f04b1795f49ec2c499c40737f530c
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-11-27 15:49:07 -0700

    Added Game object for state management

---
+++ src/Concrete/Game.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+#include "ofVec2f.h"
+
+Game::Game(ofVec2f size) {
+    bounds = ofRectangle(0,0,size.x,size.y);
+};
+
+void Game::startGame() {};
+
+void Game::updateGame() {};
+void Game::drawGame() {};
+
+void Game::exitGame() {
+    dispose();
+};
+void Game::dispose() {};
