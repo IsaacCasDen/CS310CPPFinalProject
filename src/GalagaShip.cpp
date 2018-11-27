a2f37b2e973450bb9a3a07a648a4b20024c8f0c4
Travis Olmstead <travis.olmstead@western.edu>
2018-11-27 09:10:15 -0700

    GalagaShip class and header file created.

---
+++ src/GalagaShip.cpp
@@ -0,0 +1,9 @@
+//
+//  GalagaShip.cpp
+//  emptyExample
+//
+//  Created by Travis Olmstead on 11/27/18.
+//
+
+#include <stdio.h>
+#include "GalagaShip.h"
---
+++ src/GalagaShip.h
@@ -0,0 +1,12 @@
+//
+//  GalagaShip.h
+//  emptyExample
+//
+//  Created by Travis Olmstead on 11/27/18.
+//
+
+#ifndef GalagaShip_h
+#define GalagaShip_h
+
+
+#endif /* GalagaShip_h */
