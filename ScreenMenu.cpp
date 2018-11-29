f7edb350d144d4d19f4ca43dbb3f4e8f248edade
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-11-29 12:59:03 -0700

    Added Menu Screen class

---
+++ ScreenMenu.cpp
@@ -0,0 +1,27 @@
+#include "ScreenMenu.h"
+
+ScreenMenu::ScreenMenu()
+{
+}
+/*
+ScreenMenu::ScreenMenu(Game game, ofVec2f size) : Screen(game,size)
+{
+	//buttonNewGame = Button("New Game", 20);
+	//buttonNewGame.setPosition(ofVec2f(100, 100));
+
+	//buttonQuit = Button("Quit", 20);
+	//buttonQuit.setPosition(ofVec2f(buttonNewGame.getBounds().getLeft(), buttonNewGame.getBounds().getBottom() + 10));
+}
+*/
+ScreenMenu::~ScreenMenu()
+{
+}
+
+void ScreenMenu::update()
+{
+}
+
+void ScreenMenu::draw()
+{
+
+}
---
+++ ScreenMenu.h
@@ -0,0 +1,24 @@
+//#ifndef SCREENMENU_H
+//#define SCREENMENU_H
+
+#pragma once
+
+#include "Screen.h"
+
+class ScreenMenu :
+	public Screen
+{
+public:
+	ScreenMenu();
+	ScreenMenu(Game game, ofVec2f size);
+	~ScreenMenu();
+
+	void update();
+	void draw();
+
+private:
+	Button buttonNewGame;
+	Button buttonQuit;
+};
+
+//#endif
