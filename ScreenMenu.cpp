269e2807a8f4b1efff71fd293e67ddad255ffe39
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-11-29 15:30:34 -0700

    Updated Main Menu button sizes

--- ScreenMenu.cpp
+++ ScreenMenu.cpp
@@ -9,9 +9,14 @@ ScreenMenu::ScreenMenu(Game *game, ofVec2f size) : Screen(game,size)
 {
 	buttonNewGame = Button("New Game", 20);
 	buttonNewGame.setPosition(ofVec2f(100, 100));
+	buttonNewGame.setWidth(200);
+	buttonNewGame.setHeight(50);
 
 	buttonQuit = Button("Quit", 20);
 	buttonQuit.setPosition(ofVec2f(buttonNewGame.getBounds().getLeft(), buttonNewGame.getBounds().getBottom() + 10));
+	buttonQuit.setWidth(200);
+	buttonQuit.setHeight(50);
+	
 }
 
 ScreenMenu::~ScreenMenu()
