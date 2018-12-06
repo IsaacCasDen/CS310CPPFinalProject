489077dc67909da069f495dedbfb6ab34d3d77a2
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-12-06 14:15:06 -0700

    Updated to include screen change backend and screen closing indicator

--- Screen.h
+++ Screen.h
@@ -4,11 +4,11 @@
 
 #include "Drawable.h"
 #include "ofColor.h"
-//#include "Button.h"
 #include "ofTrueTypeFont.h"
 #include <string>
 #include "ofVec2f.h"
 #include "ofVec3f.h"
+#include "ofEvent.h"
 
 class Game;
 
@@ -16,6 +16,9 @@ class Screen :
 	public Drawable
 {
 public:
+
+	ofEvent<void> closed;
+
 	Screen();
 	Screen(Game *game, ofVec2f size);
 	~Screen();
@@ -23,13 +26,23 @@ public:
 	virtual void mouseMoved(int x, int y) = 0;
 	virtual void mousePressed(int x, int y, int button) = 0;
 	virtual void mouseReleased(int x, int y, int button) = 0;
+	Screen * takePreLoadedScreen();
+	bool hasPreLoadedScreen();
+	bool isGameRunning();
+	void setGameRunning(bool value);
+	bool isClosed();
 
 protected:
 	Game * getGame();
+	void setPreLoadedScreen(Screen * screen);
+	void onClosed();
 	
 private:
-	Game * game;
+	Game * game = nullptr;
 	void setGame(Game *game);
+	Screen * preLoadedScreen = nullptr;
+	bool _isGameRunning = false;
+	bool _isClosed = false;
 
 };
 
