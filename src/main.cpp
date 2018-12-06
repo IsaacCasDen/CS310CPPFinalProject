a3a19cfe6919b2240736fe9f07c1df97fc5307e2
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-12-06 14:11:46 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofVec2f.h"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofVec2f size = ofVec2f(1280,720);
+	ofSetupOpenGL(size.x,size.y,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
