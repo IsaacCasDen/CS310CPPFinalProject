19d90ced7d4492f41794f1e9a140596ad1ab636d
Isaac Casteel-Denney <isaac.denney@western.edu>
2018-12-13 16:18:32 -0700

    Added class functionality
    * Added overlay colors for SpriteObjects
    * Added uint_fast64_t id for equality checking
    * Added support for multiple sprite sets bounded by vector positions

--- SpriteObject.cpp
+++ SpriteObject.cpp
@@ -1,6 +1,14 @@
 #include "SpriteObject.h"
 
+ofColor SpriteObject::getOverlayColor()
+{
+	return overlayColor;
+}
 
+void SpriteObject::setOverlayColor(ofColor color)
+{
+	overlayColor = color;
+}
 
 SpriteObject::SpriteObject(ofRectangle gameBounds)
 {
@@ -11,10 +19,14 @@ SpriteObject::~SpriteObject()
 {
 }
 
+uint_fast64_t SpriteObject::getObjectId()
+{
+	return objectId;
+}
+
 ofImage SpriteObject::getSprite()
 {
 	ofImage value;
-
 	if (sprites.size() > spriteIndex) {
 		value = sprites[spriteIndex];
 	}
@@ -30,10 +42,40 @@ void SpriteObject::keyReleased(int key)
 {
 }
 
+int SpriteObject::getSpriteSetStart()
+{
+	return spriteSetStart;
+}
+
+void SpriteObject::setSpriteSetStart(int value)
+{
+	if (value < sprites.size() && value >= 0)
+		spriteSetStart = value;
+}
+
+int SpriteObject::getSpriteSetEnd()
+{
+	return spriteSetEnd;
+}
+
+void SpriteObject::setSpriteSetEnd(int value)
+{
+	if (value < sprites.size() && value >= 0)
+		spriteSetEnd = value;
+}
+
 void SpriteObject::nextSprite()
 {
 	if (sprites.size() > 0) {
-		spriteIndex = (spriteIndex + 1) % sprites.size();
+		spriteIndex++;
+		//spriteIndex = spriteIndex % (spriteSetEnd + 1 < sprites.size()) ? spriteSetEnd + 1 : sprites.size();
+		if ((spriteSetEnd + 1 < sprites.size()))
+			spriteIndex = spriteIndex %  spriteSetEnd + 1;
+		else
+			spriteIndex = spriteIndex % sprites.size();
+		
+		if (spriteIndex < spriteSetStart)
+			spriteIndex = spriteSetStart + (abs(spriteSetStart - spriteIndex));
 	}
 }
 
